33ae66ce6c38f9641721c02e4956ca1edde01c2c
Jameson Miller <jamill@microsoft.com>
2015-03-03 22:13:18 -0500

    fix typo in error message

--- LibGit2Sharp/Repository.cs
+++ LibGit2Sharp/Repository.cs
@@ -672,7 +672,7 @@ namespace LibGit2Sharp
 
                         if (!continueOperation)
                         {
-                            throw new UserCancelledException("Clone of submoudule cancelled by user.");
+                            throw new UserCancelledException("Recursive clone of submodules was cancelled.");
                         }
 
                         repo.Submodules.Update(sm.Name, updateOptions);
